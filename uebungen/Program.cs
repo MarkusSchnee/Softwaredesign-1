87f31c7ba31d2301b21d9349093bb0c591238cd9
Chris-hfu <christoph@latocha.de>
2018-10-24 11:00:42 +0200

    uebungen

---
+++ uebungen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uebungen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            double pi = 3.14;
+            string salute = "";
+            var variable1 = 0;
+            var variable2 = 0.5f;  //float
+            var variable3 = 0.5d;  //double
+            var variable4 = 5; 
+
+            Console.WriteLine(variable4);
+
+        }
+    }
+}
