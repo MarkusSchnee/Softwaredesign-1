3e0cfe3c8fc9dff5500683068b247ca73cd54d8b
Chris <christoph@latocha.de>
2018-11-02 16:33:18 +0100

    aufgabe3

---
+++ Aufgabe03/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace aufgabe03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gib eine Zahl die umgerechnet werden soll");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("auswelchem zahlensystem stammt die zahl? Bitte als zahl eingeben. (z.B. aus dem Dezimal ist es eine 10)");
+            int fromSystem = int.Parse(Console.ReadLine());
+            Console.WriteLine("in welches zahlensystem soll umgerechnet werden? Bitte als zahl eingeben. (z.B. aus dem Dezimal ist es eine 10)");
+            int toSystem = int.Parse(Console.ReadLine());       
+            Console.WriteLine("Das Ergebnis ist: " + ConvertNumberFromSystemToSystem(number, fromSystem, toSystem));
+            
+        }
+
+         static int ConvertNumberFromSystemToSystem(int number, int fromSystem, int toSystem)
+            {
+                int result = 0;
+                result = OtherToDecimal(number, fromSystem);
+                result = DecimalToOther(result, toSystem);
+                return result;
+            }
+
+           static int DecimalToOther(int dec, int system)
+            {
+                int result = 0;
+                int factor = 1;
+                while (dec != 0)
+                {
+                    int digit = dec % system;
+                    dec /= system;
+                    result += factor * digit;
+                    factor *= 10;
+                }
+                return result;
+            }
+
+            static int OtherToDecimal(int other, int system)
+            {
+                int result = 0;
+                int factor = 1;
+                while (other != 0)
+                {
+                    int digit = other % 10;
+                    other /= 10;
+                    result += factor * digit;
+                    factor *= system;
+                }
+                return result;
+            }
+
+
+
+
+    }
+}
