43276fe58b37eab67ff02118a837184dcd168285
Chris <christoph@latocha.de>
2018-11-15 21:55:39 +0100

    test

--- Aufgabe05/Program.cs
+++ Aufgabe05/Program.cs
@@ -8,31 +8,46 @@ namespace Aufgabe05
 {
    class Program
     {
-        public static char[] reserveString(string input)
+         static void ReverseWhole(string input)
         {
-            char[] output = new char[input.Length];
- 
-            for (int i = 1; i <= input.Length; i++)
-            {
-                output[i-1] = input[input.Length-i];
-            }
- 
-            return output;
+            char[] reverse = input.ToCharArray();
+            Array.Reverse(reverse);
+            input = String.Join("", reverse);
+            Console.Write(input+"\n");
+         }
+
+        static void ReversePosition(string input)
+        {
+            string [] words = input.Split(' ');
+            Array.Reverse(words);
+            input = String.Join(" ", words);
+            Console.Write(input+"\n");        
         }
+
+        static void ReverseLettersinWords(string input)
+        {   string reversed = "";
+            string [] word = input.Split(' ');
+            for (int i =0; i < word.Length;i++)
+            {
+                string tempString = word[i];
+                char[] tempArray = tempString.ToCharArray();
+                Array.Reverse(tempArray);
+                input = String.Join("", tempArray);
+                reversed +=input+" ";
+            }   
+            Console.Write(reversed);
+        }
+
+       
  
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a string!");
             string inputString = Console.ReadLine();
-            char[] outputString = reserveString(inputString);
- 
-            for(int i = 0; i < inputString.Length-1; i++)
-            {
-                Console.Write(outputString[i]);
-            }
- 
-            Console.Write(outputString[inputString.Length - 1]);
-           
+            Console.Write("\n");
+            ReverseWhole(inputString);  
+            ReversePosition(inputString);         
+            ReverseLettersinWords(inputString);
         }
     }
 }
