e5370b18d09a0b65eae90aaff85c773188d9465c
Chris <christoph@latocha.de>
2018-11-15 21:58:29 +0100

    format

--- Aufgabe05/Program.cs
+++ Aufgabe05/Program.cs
@@ -3,47 +3,48 @@
 
 namespace Aufgabe05
 {
-   class Program
+    class Program
     {
-         static void ReverseWhole(string input)
+        static void ReverseWhole(string input)
         {
             char[] reverse = input.ToCharArray();
             Array.Reverse(reverse);
             input = String.Join("", reverse);
-            Console.Write(input+"\n");
-         }
+            Console.Write(input + "\n");
+        }
 
         static void ReversePosition(string input)
         {
-            string [] words = input.Split(' ');
+            string[] words = input.Split(' ');
             Array.Reverse(words);
             input = String.Join(" ", words);
-            Console.Write(input+"\n");        
+            Console.Write(input + "\n");
         }
 
         static void ReverseLettersinWords(string input)
-        {   string reversed = "";
-            string [] word = input.Split(' ');
-            for (int i =0; i < word.Length;i++)
+        {
+            string reversed = "";
+            string[] word = input.Split(' ');
+            for (int i = 0; i < word.Length; i++)
             {
                 string tempString = word[i];
                 char[] tempArray = tempString.ToCharArray();
                 Array.Reverse(tempArray);
                 input = String.Join("", tempArray);
-                reversed +=input+" ";
-            }   
+                reversed += input + " ";
+            }
             Console.Write(reversed);
         }
 
-       
- 
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a string!");
             string inputString = Console.ReadLine();
             Console.Write("\n");
-            ReverseWhole(inputString);  
-            ReversePosition(inputString);         
+            ReverseWhole(inputString);
+            ReversePosition(inputString);
             ReverseLettersinWords(inputString);
         }
     }
