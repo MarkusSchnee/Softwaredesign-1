60ed6749e9c9c2639aa60afbe301345a1cdd42eb
Chris <christoph@latocha.de>
2018-11-17 15:37:07 +0100

    Aufgabe05

--- Aufgabe05/Program.cs
+++ Aufgabe05/Program.cs
@@ -1,52 +1,48 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Aufgabe05
 {
     class Program
     {
-
+        public static String result;
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a string!");
-            string inputString = Console.ReadLine();
-            Console.Write("\n");
-            ReverseWhole(inputString);
-            ReversePosition(inputString);
-            ReverseLettersinWords(inputString);
+            Console.WriteLine("Bitte einen kleinen Satz eingeben");
+            Console.Write("> ");
+            var text = Console.ReadLine();
+            string letters = reverseLetters(text);
+            string words = reverseWords(text);
+            string sentence = reverseSentence(text);
+            Console.WriteLine(sentence + "\n" + words + "\n" + letters);
         }
 
-        static void ReverseWhole(string input)
+        public static String reverseLetters(String text)
         {
-            char[] reverse = input.ToCharArray();
-            Array.Reverse(reverse);
-            input = String.Join("", reverse);
-            Console.Write(input + "\n");
-        }
-
-        static void ReversePosition(string input)
-        {
-            string[] words = input.Split(' ');
-            Array.Reverse(words);
-            input = String.Join(" ", words);
-            Console.Write(input + "\n");
-        }
-
-        static void ReverseLettersinWords(string input)
-        {
-            string reversed = "";
-            string[] word = input.Split(' ');
-            for (int i = 0; i < word.Length; i++)
+            List<String> letters = new List<String>();
+            letters.Add(text);
+            foreach (var letter in letters)
             {
-                string tempString = word[i];
-                char[] tempArray = tempString.ToCharArray();
-                Array.Reverse(tempArray);
-                input = String.Join("", tempArray);
-                reversed += input + " ";
+                result = String.Join("", letter.ToString().Reverse());
             }
-            Console.Write(reversed);
+            return result;
         }
 
+        public static String reverseWords(String text)
+        {
+            string[] words = text.Split(' ');
+            Array.Reverse(words);
+            text = String.Join(" ", words);
+            return text;
+        }
 
+        public static String reverseSentence(String text)
+        {
+            var reversedWords = string.Join(" ", text.Split(' ')
+            .Select(x => new String(x.Reverse().ToArray()))
+            .ToArray());
+            return reversedWords;
+        }
     }
 }
