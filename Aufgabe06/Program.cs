292d37f7247f1922000f78369d0295b6807ebc98
Chris <christoph@latocha.de>
2018-11-23 15:08:46 +0100

    test

---
+++ Aufgabe06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aufgabe06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quizelement quiz1 = new Quizelement();
+            Console.WriteLine(quiz1);
+            quiz1.question = "Wer war der erste Bundeskanzler";
+            quiz1.answer = new string[] {"Brack Obama", "Helmut Kohl", "Konrad Adenauer", "Angelo Merkel"};
+            quiz1.correct = 'c';
+             Console.WriteLine(quiz1.question);
+        }
+    }
+}
---
+++ Aufgabe06/Quiyelement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aufgabe06
+{
+
+    public class Quizelement
+    {
+        //Was ist ein Assambly
+
+        public string question;
+        public string [] answer;
+        public char correct;
+
+        
+        public Boolean IsAnswerCorrect(char choise){
+        
+            return (choise == this.correct);
+    
+        }
+
+        void Show(){
+            Console.WriteLine(question);
+            Console.WriteLine(answer);
+        }
+    }
+}
